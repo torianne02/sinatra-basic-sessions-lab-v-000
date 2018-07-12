d1ddd65a09a558d6a7376e27be02510c99f31078
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-12 21:54:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    @sessions = session
+
+    erb :index
+  end
+
+  post '/checkout' do
+    @item = params[:item]
+    session[:item] = params[:item]
+
+    erb :checkout
+  end
 end
